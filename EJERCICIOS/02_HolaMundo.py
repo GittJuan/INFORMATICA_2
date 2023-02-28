1fe7ed16e9086b9a333d60a7deeb34cd994d597c
GittJuan <jrodriguezrodr@unal.edu.co>
2023-02-28 10:15:38 -0500

    Actualizacion Repositorio

---
+++ EJERCICIOS/02_HolaMundo.py
@@ -0,0 +1,40 @@
+# Ejercicios
+
+# Imprimir la siguiente información 
+# "Hola mundo"
+# "Hola mundo cruel"
+# 10 
+# -9999
+# True
+# False
+# [1, 2, 3]
+# (10, 20, 30)
+# {"hello": "hola", "world": "mundo", "crueles":"cruel"}
+
+#Solucion
+print("Hola mundo")
+print("Hola mundo cruel")
+print(10)
+print(-9999)
+print(True)
+print(False)
+print([ 1, 2, 3])
+print(( 10, 20, 30))
+print({"hello":"hola", "world":"mundo", "crueles":"cruel"})
+
+
+# Imprimir los siguientes enteros en una misma linea
+#  10
+#  -90
+#  101
+
+print(10, 90, 101)
+
+# Imprimir las siguientes cadenas en una misma linea
+# las cadenas deben estar separadas por dos guiones "--"
+# "Anita"
+# "Lava"
+# "La "
+# "Tina"
+
+print("Anita","Lava","La ","Tina", sep = " -- ")
