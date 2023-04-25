38bbccd77dca859b128aacea67e2bd5777d98422
GittJuan <jrodriguezrodr@unal.edu.co>
2023-04-25 12:15:40 -0500

    entrega informe 2

---
+++ EJERCICIOS/11_GraficasMatplotlib.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Dispersion de puntos
+x = [1,2,3,4,5,6]
+y = [10,20,30,40,50, 60]
+plt.figure()
+plt.plot(x, y, "o")
+plt.show()
+
+#Dispersion senoidal más elaborada
+x = np.linspace(0,6.28)
+y = np.sin(x)
+plt.figure()
+plt.plot(x,y, "r+")
+plt.title("Funcion senoidal")
+plt.xlabel("Angulo")
+plt.ylabel("sin(x)")
+plt.grid()
+plt.show()
